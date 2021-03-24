fcfb8f83d670ae71e8027ab531f425af234a7473
weiyizheng <weiyizheng@chehang168.com>
2021-03-24 14:14:09 +0800

    wyz

--- api/views.py
+++ api/views.py
@@ -6,9 +6,10 @@
 # @desc :
 from django.http import HttpResponse, request
 import json
-from api.models import SaasScPoster
+from api.models import SaasScPoster, ChUser
 from api.basemodels.SaasPoster import SaasPoster
 from django.conf import settings
+from django.db.models import Count
 
 
 def hello(request):
@@ -20,10 +21,43 @@ def hello(request):
 
 # Django自带的数据库操作
 def list(request):
+    # ch_user 新增一条
+    # user = ChUser(name='weiyizheng', phone='13521282025')
+    # user.save()
+    # user = ChUser.objects.create(name='heiue', phone='13521282025')
+    # print(user.id)
+
+    """ 修改 """
+    # user = ChUser.objects.get(id=1)
+    # user.name = 'wyz'
+    # user.save()
+
+    # id = ChUser.objects.filter(id=1).update(name='wyzheng')
+    # print(id)
+
+    """ 删除 """
+    # user = ChUser.objects.get(id=1)
+    # user.delete()
+
+    ChUser.objects.filter(id=2).delete()
+
+    """ 获取一条 """
+    one = ChUser.objects.get(id=3)
+    print(one.name)
+
+    """ 过滤 """
     data = {}
-    lists = SaasScPoster.objects.filter(id__lt=10).values()
+    lists = ChUser.objects.order_by("-id").exclude(id__gt=5).values()
+    # lists = ChUser.objects.order_by("-id").filter(id__lt=10).values()
+    data = []
+    for item in lists:
+        data.append(item)
     # print(lists)
-    return HttpResponse()
+
+    """ 聚合 """
+    count = ChUser.objects.aggregate(Count('id'))
+    print(count)
+    return HttpResponse(json.dumps(data))
 
 
 def list2(request):
